89c690895c998e02064a2f4822954385c28a1db7
Akshaysat <akshay.satpaise99@gmail.com>
2023-05-08 16:34:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(
 
 st.write("-----")
 
-strategy_name = st.selectbox("Select the Strategy", ["STS", "CPS", "STBT-BN"]).lower()
+strategy_name = st.selectbox("Select the Strategy", ["STS", "CPS"]).lower()
 
 # connect to the database
 mongo = MongoClient(st.secrets["mongo_db"]["mongo_url"])
